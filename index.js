0b013639167b0bce670da1a5eddcc97810ed1a7c
Muhammad Rizky Putra <59104449+yudha2003@users.noreply.github.com>
2023-06-23 18:36:33 +0700

    Update index.js

--- index.js
+++ index.js
@@ -109,66 +109,6 @@ client.on("message_ack", (msg, ack) => {
     // The message was read
   }
 });
-
-client.on("group_join", (notification) => {
-  // User has joined or been added to the group.
-  console.log("join", notification);
-  notification.reply("User joined.");
-});
-
-client.on("group_leave", (notification) => {
-  // User has left or been kicked from the group.
-  console.log("leave", notification);
-  notification.reply("User left.");
-});
-
-client.on("group_update", (notification) => {
-  // Group picture, subject or description has been updated.
-  console.log("update", notification);
-});
-
-client.on("change_state", (state) => {
-  console.log("CHANGE STATE", state);
-});
-
-// Change to false if you don't want to reject incoming calls
-let rejectCalls = true;
-
-client.on("call", async (call) => {
-  console.log("Call received, rejecting. GOTO Line 261 to disable", call);
-  if (rejectCalls) await call.reject();
-  await client.sendMessage(
-    call.from,
-    `[${call.fromMe ? "Outgoing" : "Incoming"}] Phone call from ${
-      call.from
-    }, type ${call.isGroup ? "group" : ""} ${
-      call.isVideo ? "video" : "audio"
-    } call. ${
-      rejectCalls ? "This call was automatically rejected by the script." : ""
-    }`
-  );
-});
-
-client.on("disconnected", (reason) => {
-  console.log("Client was logged out", reason);
-});
-
-client.on("contact_changed", async (message, oldId, newId, isContact) => {
-  /** The time the event occurred. */
-  const eventTime = new Date(message.timestamp * 1000).toLocaleString();
-
-  console.log(
-    `The contact ${oldId.slice(0, -5)}` +
-      `${
-        !isContact
-          ? " that participates in group " +
-            `${(await client.getChatById(message.to ?? message.from)).name} `
-          : " "
-      }` +
-      `changed their phone number\nat ${eventTime}.\n` +
-      `Their new phone number is ${newId.slice(0, -5)}.\n`
-  );
-
   /**
    * Information about the {@name message}:
    *
@@ -191,18 +131,4 @@ client.on("contact_changed", async (message, oldId, newId, isContact) => {
    * the old (before the change) and a new one, stored in alphabetical order.
    * {@name message.from} is a current user's id that has a chat with a user,
    * whos phone number was changed.
-   * {@name message.to} is a user's id (after the change), the current user has a chat with.
-   */
-});
-
-client.on("group_admin_changed", (notification) => {
-  if (notification.type === "promote") {
-    /**
-     * Emitted when a current user is promoted to an admin.
-     * {@link notification.author} is a user who performs the action of promoting/demoting the current user.
-     */
-    console.log(`You were promoted by ${notification.author}`);
-  } else if (notification.type === "demote")
-    /** Emitted when a current user is demoted to a regular user. */
-    console.log(`You were demoted by ${notification.author}`);
-});
+**/
